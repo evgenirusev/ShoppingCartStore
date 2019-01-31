e79671b9983d118ee15c1b84b29fa3a267ae947c
rusev9 <evg.rusev@gmail.com>
2019-01-31 08:30:49 +0200

    Products presentation layer

--- src/Common/ShoppingCartStore.Common/ViewModels/Product/ProductViewModel.cs
+++ src/Common/ShoppingCartStore.Common/ViewModels/Product/ProductViewModel.cs
@@ -5,6 +5,7 @@
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string ImageURI { get; set; }
         public decimal Price { get; set; }
     }
 }
