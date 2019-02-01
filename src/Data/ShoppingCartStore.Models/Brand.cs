b02636df8b407938fdd3643539e1ff53b44efd2b
rusev9 <evg.rusev@gmail.com>
2019-02-01 15:02:53 +0200

    Category & Brand entities

---
+++ src/Data/ShoppingCartStore.Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCartStore.Models
+{
+    public class Brand : BaseModel
+    {
+        public Brand()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ src/Data/ShoppingCartStore.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ShoppingCartStore.Models
+{
+    public  class Category
+    {
+        public Category()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
