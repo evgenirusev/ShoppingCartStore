5af5cd10f22ac14c64a3447578a5baab0445dcee
rusev9 <evg.rusev@gmail.com>
2019-02-03 20:14:09 +0200

    Add to wishlist service layer

---
+++ src/Services/ShoppingCartStore.Services.DataServices/IWishlistService.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCartStore.Services.DataServices
+{
+    using System.Threading.Tasks;
+
+    public interface IWishlistService
+    {
+        Task AddToWishlistAsync(string productId, string customerId);
+    }
+}
---
+++ src/Services/ShoppingCartStore.Services.DataServices/Implementations/WishlistService.cs
@@ -0,0 +1,53 @@
+﻿namespace ShoppingCartStore.Services.DataServices.Implementations
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using Microsoft.AspNetCore.Identity;
+    using ShoppingCartStore.Data.Common.Repositories;
+    using ShoppingCartStore.Models;
+
+    public class WishlistService : BaseService<Wishlist>, IWishlistService
+    {
+        public WishlistService(IRepository<Wishlist> repository, IMapper mapper
+            , UserManager<Customer> userManager)
+            : base(repository, mapper, userManager)
+        {
+        }
+
+        public async Task AddToWishlistAsync(string productId, string customerId)
+        {
+            var wishlist = await this.GetWishListAsync(customerId);
+
+            ProductsWishlists pw = new ProductsWishlists();
+            pw.ProductId = productId;
+            pw.WishlistId = wishlist.Id;
+
+            wishlist.ProductsWishlists.Add(pw);
+        }
+
+        private async Task<Wishlist> GetWishListAsync(string customerId)
+        {
+            var wishlist = this.Repository.All()
+                .Where(wl => wl.CustomerId == customerId).FirstOrDefault();
+
+            if (wishlist == null)
+            {
+                wishlist = await this.CreateAndRetrieveAsync(customerId);
+            }
+
+            return wishlist;
+        }
+
+        private async Task<Wishlist> CreateAndRetrieveAsync(string customerId)
+        {
+            Wishlist wishlist = new Wishlist();
+            wishlist.CustomerId = customerId;
+
+            await Repository.AddAsync(wishlist);
+            await Repository.SaveChangesAsync();
+
+            return wishlist;
+        }
+    }
+}
